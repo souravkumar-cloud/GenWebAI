b6127c81d35bac9fa4705e885e8413267f8175a9
Bala <132751639+souravkumar-cloud@users.noreply.github.com>
2026-04-06 15:50:10 +0530

    Set axios to use credentials for requests
    Configure axios to include credentials with requests.

--- clinet/src/main.jsx
+++ clinet/src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
+import axios from "axios";
+axios.defaults.withCredentials=true;
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
