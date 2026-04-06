94f68701a11f33c449cc9a46e7aa12e5cbaa8a22
Bala <132751639+souravkumar-cloud@users.noreply.github.com>
2026-04-06 15:47:01 +0530

    Update cookie settings in auth controller
    Added 'path' option to cookie settings.

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -21,7 +21,8 @@ export const googleAuth=async(req,res)=>{
             httpOnly:true,
             secure:true,
             sameSite:"None",
-            maxAge:7*24*60*60*1000
+            maxAge:7*24*60*60*1000,
+            path:"/"
         })
         return res.status(200).json(user)
     }catch(error){
