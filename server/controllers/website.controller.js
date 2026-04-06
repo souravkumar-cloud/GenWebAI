f669cb5ea872692c6637f4b38e5550ceffdeef50
Bala <132751639+souravkumar-cloud@users.noreply.github.com>
2026-04-06 15:17:02 +0530

    Refactor getBySlug to remove user ID filter
    Removed user ID check from getBySlug function.

--- server/controllers/website.controller.js
+++ server/controllers/website.controller.js
@@ -365,8 +365,7 @@ export const deploy=async(req,res)=>{
 export const getBySlug=async(req,res)=>{
   try {
     const website=await Website.findOne({
-      slug:req.params.slug,
-      user:req.user._id
+      slug:req.params.slug
     })
     if(!website) return res.status(400),json({message:"website not found"})
     return res.status(200).json(website)
