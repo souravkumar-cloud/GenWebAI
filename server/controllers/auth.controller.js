3a0b4e71855161086cdbafb284158023d721b904
Bala <132751639+souravkumar-cloud@users.noreply.github.com>
2026-04-06 15:41:27 +0530

    Fix sameSite attribute value for cookies

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -20,7 +20,7 @@ export const googleAuth=async(req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             secure:true,
-            sameSite:"none",
+            sameSite:"None",
             maxAge:7*24*60*60*1000
         })
         return res.status(200).json(user)
@@ -34,7 +34,7 @@ export const logOut=async(req,res)=>{
         res.clearCookie("token",{
             httpOnly:true,
             secure:true,
-            sameSite:"none"
+            sameSite:"None"
         })
         return res.status(200).json({message:"log out successfully"})
     }catch(error){
