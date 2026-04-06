4765d34516f3007f3a5208e96dfc49f826a90028
Bala <132751639+souravkumar-cloud@users.noreply.github.com>
2026-04-06 14:37:27 +0530

    Update cookie security settings in auth controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -19,8 +19,8 @@ export const googleAuth=async(req,res)=>{
 
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"lax",
+            secure:true,
+            sameSite:"none",
             maxAge:7*24*60*60*1000
         })
         return res.status(200).json(user)
@@ -33,8 +33,8 @@ export const logOut=async(req,res)=>{
     try{
         res.clearCookie("token",{
             httpOnly:true,
-            secure:false,
-            sameSite:"strict"
+            secure:true,
+            sameSite:"none"
         })
         return res.status(200).json({message:"log out successfully"})
     }catch(error){
